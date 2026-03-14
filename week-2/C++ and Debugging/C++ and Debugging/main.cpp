6621c86e70b29aa32bb503a0e5b7448788fdf7b0
Adam Stark <adamstark.uk@gmail.com>
2026-03-14 19:08:59 +0000

    Pointers and References

--- week-2/C++ and Debugging/C++ and Debugging/main.cpp
+++ week-2/C++ and Debugging/C++ and Debugging/main.cpp
@@ -6,21 +6,82 @@
 //
 
 #include <iostream>
-#include "Classes/Triangle.h"
-#include "Classes/Rectangle.h"
 
-void printShape (Shape& s)
+void pointers()
 {
-    std::cout << s.getName() << " has area of " << s.getArea() << std::endl;
+    int a = 3;
+    int b = 7;
+    
+    int* ptr = &b;
+    
+    // Print out memory address of a
+    std::cout << &a << std::endl;
+    
+    // Print out pointer
+    std::cout << ptr << std::endl;
+    
+    // "De-reference" pointer and print out value of the location that the pointer points to
+    std::cout << *ptr << std::endl;
+    
+    // Print b before writing to dereferenced pointer
+    std::cout << b << std::endl;
+    
+    *ptr = 11;
+    
+    // Print b after writing to dereferenced pointer
+    std::cout << b << std::endl;
+    
+    // set a point to null
+    ptr = nullptr;
+}
+
+void references()
+{
+    int a = 3;
+    
+    int& c = a; // 'ref' is an 'alias' for 'a'
+    
+    std::cout << a << std::endl;
+    
+    c = 2;
+    
+    std::cout << a << std::endl;
+}
+
+
+class Counter
+{
+public:
+    
+    void increaseValue()
+    {
+        count += 1;
+    }
+    
+    int getCount()
+    {
+        return count;
+    }
+    
+private:
+    
+    int count {0};
+};
+
+void processCounter (Counter& c)
+{
+    c.increaseValue();
+    c.increaseValue();
+    c.increaseValue();
 }
 
 int main(int argc, const char * argv[])
 {
-    Rectangle r (20, 50);
-    Triangle t (4, 5, 6);
+    Counter c;
     
-    printShape (r);
-    printShape (t);
+    processCounter (c);
+    
+    std::cout << "COUNT IS: " << c.getCount() << std::endl;
     
     return EXIT_SUCCESS;
 }
